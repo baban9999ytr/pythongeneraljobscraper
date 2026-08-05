513d244ac881de37ae5cae22b1eff7d9757dbef9
Mustafa Göksal <gogsalmustafa19@gmail.com>
2026-08-05 11:07:07 +0300

    Load environment variables for host and port

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+import os
 import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = int(os.getenv("PORT", 8000)) 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=2, log_level="info")
+    uvicorn.run("main:app", host=HOST, port=PORT, workers=2, log_level="info")
